185d857b72ac664d65bf023f726aac2031c781b6
Fernando Gotardi <fernando.gotardi@gmail.com>
2024-05-18 13:28:08 -0300

    Configuracao pipeline

--- app/app.py
+++ app/app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def home():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
